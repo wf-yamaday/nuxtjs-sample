3baa21e354017da5a6334224e83910a0143e0c6e
wf-yamaday <e155417k@st.u-gakugei.ac.jp>
2018-09-26 20:01:46 +0900

    sample project

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navbar">
+        <div class="container">
+            <div class="navbar-brand">
+                <nuxt-link to="/breeds" class="navbar-item">犬</nuxt-link>
+                <span class="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+            </div>
+        </div>
+    </nav>
+</template>
